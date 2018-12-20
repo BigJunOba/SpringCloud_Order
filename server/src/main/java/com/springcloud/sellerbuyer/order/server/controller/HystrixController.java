b3f2e3b80ceabb7d1670d296567220087d354f5a
BigJunOba <sjy534948129@sina.com>
2018-12-20 14:47:39 +0800

    使用Hystrix配置超时时间设置

--- server/src/main/java/com/springcloud/sellerbuyer/order/server/controller/HystrixController.java
+++ server/src/main/java/com/springcloud/sellerbuyer/order/server/controller/HystrixController.java
@@ -1,6 +1,8 @@
 package com.springcloud.sellerbuyer.order.server.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,14 @@ import java.util.Arrays;
  * @create: 2018-12-20 11:15
  */
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class HystrixController {
 
-    @HystrixCommand(fallbackMethod = "fallback")
+//    @HystrixCommand(fallbackMethod = "fallback")
+    // 超时时间配置，配置成3秒，3秒之后如果没有请求到，就触发降级
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     @GetMapping("/getProductInfoList")
     public String getProductInfoList() {
         // 1. 调用其他服务时，服务降级
@@ -31,5 +38,9 @@ public class HystrixController {
 
     private String fallback() {
         return "fallback提示:太拥挤了，请稍后再试！";
+    }
+
+    private String defaultFallback() {
+        return  "默认defaultFallback提示";
     }
 }
