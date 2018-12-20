b371d712047fccfe1af570d45f0d392eaa4b3245
BigJunOba <sjy534948129@sina.com>
2018-12-20 16:43:33 +0800

    使用hystrix-dashboard实现服务降级

--- server/src/main/java/com/springcloud/sellerbuyer/order/server/OrderApplication.java
+++ server/src/main/java/com/springcloud/sellerbuyer/order/server/OrderApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.springcloud.sellerbuyer.product.client")
 @EnableCircuitBreaker
+@ComponentScan(basePackages = "com.springcloud.sellerbuyer")
 public class OrderApplication {
 
     public static void main(String[] args) {
