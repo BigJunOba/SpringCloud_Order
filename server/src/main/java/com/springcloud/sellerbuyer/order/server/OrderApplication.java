2978d749d6955ae3bf0c010d6996d2adaddde532
BigJunOba <sjy534948129@sina.com>
2018-12-20 17:03:35 +0800

    添加hystrix-dashboard组件可视化分析服务熔断

--- server/src/main/java/com/springcloud/sellerbuyer/order/server/OrderApplication.java
+++ server/src/main/java/com/springcloud/sellerbuyer/order/server/OrderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = "com.springcloud.sellerbuyer.product.client")
 @EnableCircuitBreaker
 @ComponentScan(basePackages = "com.springcloud.sellerbuyer")
+@EnableHystrixDashboard
 public class OrderApplication {
 
     public static void main(String[] args) {
